e809ce0b2025e6aff99819056cf5db92742734ce
patekvn1-del <patekvn1@gmail.com>
2026-03-14 23:22:05 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import yt_dlp
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CallbackQueryHandler, MessageHandler, ContextTypes, filters
 
-TOKEN = os.environ.get("BOT_TOKEN")
+TOKEN = os.environ["BOT_TOKEN"]
 
 user_links = {}
 
